9ee8d4dc421282ebdbd601eb4f6336f8bd2fb1de
Sean Angle <sla88@txstate.edu>
2023-08-28 18:50:32 -0500

    hid my instagram and facebook from footer as well

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,8 +17,8 @@ export const Footer = () => {
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
                 <a href="https://www.linkedin.com/in/sean-angle/" target="_blank"><img src={navIcon1} alt="" /></a>
-                <a href="https://www.facebook.com/sean.angle.5/" target="_blank"><img src={navIcon2} alt="" /></a>
-                <a href="https://www.instagram.com/seanangle_/" target="_blank"><img src={navIcon3} alt="" /></a>
+                {/* <a href="https://www.facebook.com/sean.angle.5/" target="_blank"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/seanangle_/" target="_blank"><img src={navIcon3} alt="" /></a> */}
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
