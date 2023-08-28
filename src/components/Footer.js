80db4e77e5c2115e85fefb5218ce7cf9c57f4893
Sean Angle <sla88@txstate.edu>
2023-08-28 14:35:20 -0500

    added social media to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,9 +16,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+                <a href="https://www.linkedin.com/in/sean-angle/" target="_blank"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.facebook.com/sean.angle.5/" target="_blank"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/seanangle_/" target="_blank"><img src={navIcon3} alt="" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
