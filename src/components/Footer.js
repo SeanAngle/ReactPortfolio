c27af913a0fb5fcf21c1f65b46de292a5826e78e
Sean Angle <sla88@txstate.edu>
2023-08-30 17:51:14 -0500

    created a build with new github link in navbar and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from "../assets/img/nav-icon4.svg";
 
 export const Footer = () => {
   return (
@@ -19,6 +20,7 @@ export const Footer = () => {
                 <a href="https://www.linkedin.com/in/sean-angle/" target="_blank"><img src={navIcon1} alt="" /></a>
                 <a href="" target="_blank"><img src={navIcon2} alt="" /></a>
                 <a href="" target="_blank"><img src={navIcon3} alt="" /></a>
+                <a href="https://github.com/SeanAngle" target="_blank"><img src={navIcon4} alt="" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
